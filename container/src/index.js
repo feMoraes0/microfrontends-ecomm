0b7a763fedbc6fc3e713e47d1df54985b751998e
feMoraes0 <fernandomoraes.lopes@gmail.com>
2023-07-29 18:08:44 -0300

    build(container): adds module federation config

--- container/src/index.js
+++ container/src/index.js
@@ -1,1 +1,8 @@
-console.log('container');
+/**
+ * This file gives webpack opportunity to load/fetch everything
+ * that is inside bootstrap before que container executes
+ * to avoid dependency missing during the execution.
+ * If bootstrap was loaded directly, an error will be thrown
+ * related with 'products' is missing or something like that.
+ */
+import('./bootstrap');
