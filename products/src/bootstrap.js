6dce50ff4441f90ebb3d0f59782a0935786513ee
feMoraes0 <fernandomoraes.lopes@gmail.com>
2023-07-31 19:13:02 -0300

    refactor(products): updates bootstrap file to be used externally without element dependency

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,10 +1,31 @@
 import faker from 'faker';
 
-let products = '';
+/**
+ * <The function name is arbitrary>
+ * Function that will handle the module and
+ * render the HTML to page.
+ * @param {*} el is an HTML element
+ */
+const mount = (el) => {
+  let products = '';
 
-for(let i = 0; i < 3; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
+  for(let i = 0; i < 3; i++) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+  }
+
+  el.innerHTML = products;
 }
 
-document.querySelector('#dev-products').innerHTML = products;
+// is in development mode
+if(process.env.NODE_ENV === 'development') {
+  const element = document.querySelector('#dev-only-products');
+  // dev-only element exists - ASSUMING (not sure about it) that container wont have the same element
+  if(element) {
+    // probably running in isolation
+    mount(element);
+  }
+}
+
+// with export the container can import an call it when necessary.
+export { mount };
