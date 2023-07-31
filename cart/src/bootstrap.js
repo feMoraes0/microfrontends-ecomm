336308fb28fafdbc021b22c72be7fd98275e397f
feMoraes0 <fernandomoraes.lopes@gmail.com>
2023-07-31 19:27:03 -0300

    refactor(cart): updates bootstrap file to be used externally without element dependency

---
+++ cart/src/bootstrap.js
@@ -0,0 +1,15 @@
+import faker from 'faker';
+
+const mount = (el) => {
+  const cartText = `<div>You have ${faker.random.number()} items in your cart.</div>`;
+  el.innerHTML = cartText;
+}
+
+if(process.env.NODE_ENV === 'development') {
+  const element = document.querySelector('#dev-only-cart');
+  if(element) {
+    mount(element);
+  }
+}
+
+export { mount };
