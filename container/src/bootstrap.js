9a5152e5ee95b8a1524b2a706c7029d25f04f7b3
feMoraes0 <fernandomoraes.lopes@gmail.com>
2023-07-31 19:19:46 -0300

    refactor(container): updates boostrap to call products mount function with container element

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,7 @@
-import 'products/ProductsIndex';
+import { mount } from 'products/ProductsIndex';
 import 'cart/CartShow';
 
 console.log('contaier');
+
+const productsElement = document.querySelector('#my-products');
+mount(productsElement);
